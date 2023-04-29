77f623895d8458991504d11220ec2c538c17f8ea
nikasenia <niks@uma.es>
2023-04-29 12:08:28 +0200

    Encrypted password, implemented cancel button and notifications

--- src/main/java/com/example/application/views/login/LoginView.java
+++ src/main/java/com/example/application/views/login/LoginView.java
@@ -2,8 +2,12 @@ package com.example.application.views.login;
 
 import com.example.application.security.AuthenticatedUser;
 import com.example.application.views.register.RegisterView;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.*;
 import com.vaadin.flow.router.internal.RouteUtil;
 import com.vaadin.flow.server.VaadinService;
@@ -12,29 +16,19 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 @AnonymousAllowed
 @PageTitle("Login")
 @Route(value = "login")
-public class LoginView extends LoginOverlay implements BeforeEnterObserver {
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 
     private final AuthenticatedUser authenticatedUser;
 
     public LoginView(AuthenticatedUser authenticatedUser) {
         this.authenticatedUser = authenticatedUser;
-        setAction(RouteUtil.getRoutePath(VaadinService.getCurrent().getContext(), getClass()));
+        Button register = new Button("Register");
 
-        LoginI18n i18n = LoginI18n.createDefault();
-        i18n.setHeader(new LoginI18n.Header());
-        i18n.getHeader().setTitle("Triis");
-        i18n.getHeader().setDescription("Login using niks/123password");
-        i18n.setAdditionalInformation(null);
-        setI18n(i18n);
+        register.addClickListener((e ->
+                register.getUI().ifPresent(ui ->
+                        ui.navigate("register"))));
 
-        setForgotPasswordButtonVisible(true);
-        setOpened(true);
-
-        RouterLink registerLink = new RouterLink("register", RegisterView.class);
-
-
-
-
+        this.add(new TriisLoginForm(this), register);
 
     }
 
@@ -42,10 +36,7 @@ public class LoginView extends LoginOverlay implements BeforeEnterObserver {
     public void beforeEnter(BeforeEnterEvent event) {
         if (authenticatedUser.get().isPresent()) {
             // Already logged in
-            setOpened(false);
             event.forwardTo("");
         }
-
-        setError(event.getLocation().getQueryParameters().getParameters().containsKey("error"));
     }
 }
--- src/main/java/com/example/application/views/register/RegisterView.java
+++ src/main/java/com/example/application/views/register/RegisterView.java
@@ -3,13 +3,17 @@ package com.example.application.views.register;
 import com.example.application.data.entities.User;
 import com.example.application.data.services.UserService;
 import com.example.application.security.AuthenticatedUser;
+import com.example.application.views.MainLayout;
+import com.example.application.views.login.LoginView;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.notification.Notification;
@@ -20,18 +24,18 @@ import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.*;
 import com.vaadin.flow.router.internal.RouteUtil;
 import com.vaadin.flow.server.VaadinService;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.shared.Registration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @AnonymousAllowed
 @PageTitle("Registration")
 @Route(value = "register")
+@RouteAlias(value = "register")
 public class RegisterView extends FormLayout {
     UserService userService;
     Binder<User> binder = new BeanValidationBinder<>(User.class);
@@ -43,7 +47,7 @@ public class RegisterView extends FormLayout {
     PasswordField confirmPassword = new PasswordField("Confirm password");
     //buttons
     Button save = new Button("Save");
-    Button close = new Button("Cancel");
+    Button close = new Button("Log in");
     private User user;
 
     public RegisterView(UserService userService) {
@@ -51,7 +55,8 @@ public class RegisterView extends FormLayout {
         //using binder to map fields to User class
         binder.bindInstanceFields(this);
         //placing the form on top of the buttons
-        VerticalLayout wholeForm = new VerticalLayout(createFormLayout(), createButtonsLayout());
+        VerticalLayout wholeForm = new VerticalLayout(new H1("REGISTRATION"),createFormLayout(), createButtonsLayout());
+        wholeForm.setMargin(true);
 
         add(wholeForm);
     }
@@ -91,6 +96,7 @@ public class RegisterView extends FormLayout {
         close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
 
         save.addClickListener(event -> register());
+        close.addClickListener(event -> UI.getCurrent().navigate(LoginView.class));
 
         //making buttons more accesible
         save.addClickShortcut(Key.ENTER);
@@ -99,6 +105,9 @@ public class RegisterView extends FormLayout {
         return new HorizontalLayout(save, close);
     }
 
+    /**
+     * VERIFY FIELDS AND SAVE USER
+     */
     private void register(){
         String userName = username.getValue();
         String password1 = password.getValue();
@@ -107,27 +116,31 @@ public class RegisterView extends FormLayout {
 
         if(userName.trim().isEmpty()){
             Notification.show("Enter a username");
+        }else if(emailValue.trim().isEmpty()){
+            Notification.show("Enter an email");
+        }else if(email.isInvalid()){
+            Notification.show("Email invalid format");
         }else if(password1 .trim().isEmpty()){
             Notification.show("Enter a password");
         }else if(password2 .trim().isEmpty()){
             Notification.show("Confirm password");
         }else if(!password1.equals(password2)){
-            Notification.show("Password don't match");
+            Notification.show("Passwords don't match");
         }else if(userService.findByUsername(userName) != null){
             Notification.show("User already exists");
         }else if(userService.findByEmail(emailValue) != null){
             Notification.show("Email already in use");
         }else{
-            userService.save(new User(userName, password1, emailValue));
+            //encrypting password
+            PasswordEncoder pass = new BCryptPasswordEncoder();
+            CharSequence passwordToEncrypt = password1;
+
+            userService.save(new User(userName, pass.encode(passwordToEncrypt), emailValue));
             Notification.show("User saved");
+
         }
 
     }
-
-
-
-
-
 
 
 
